5061209c901714782d7479f466152b40dfbd859d
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-17 22:27:58 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "M", "Brown", "Sweet", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "M", "White", "Rowdy", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "dark brown", "Calm", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 7, "M", "Brown", "goofy", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "F", "Black", "Moody", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "M", "Brown", "Angry", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "F", "dark brown", "goofy", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Null, 3, "M", "dark brown", "Viscious", "1");
